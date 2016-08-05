403000d30d9e43696300e56d9ad821cccadc714e
Ola <oladipupo.popoola@gmail.com>
2016-08-05 09:44:22 +0100

    updated admin ui and templates

--- CabAdmin/assets/js/app.js
+++ CabAdmin/assets/js/app.js
@@ -1,14 +1,13 @@
-(function () {
+(function() {
 	// Initialize Firebase
   var config = {
     apiKey: "AIzaSyDhrhS7R0XUA9h5V4YCQI6FVuRz625dAQM",
     authDomain: "cabbooking-55f03.firebaseapp.com",
     databaseURL: "https://cabbooking-55f03.firebaseio.com",
-    //storageBucket: "",
+    storageBucket: "cabbooking-55f03.appspot.com",
   };
   firebase.initializeApp(config);
 
-  var provider = new firebase.auth.FacebookAuthProvider();
 
   var fName = document.getElementById('fname');
   var lname = document.getElementById('lname');
@@ -60,25 +59,6 @@
 	  });
   }
 
-  btnFacebook.addEventListener('click', e => {
-  	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
-});
-
   //Add a firebase realtime listener if user exists
   firebase.auth().onAuthStateChanged(firebase => {
   	if (firebaseUser) {
@@ -90,4 +70,5 @@
 
 }());
   
+
     
